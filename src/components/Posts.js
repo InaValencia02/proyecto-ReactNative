0b22545deb7543c309980bf9b0926f6bbe8d2765
InaValencia02 <ivalenciaamaya@udesa.edu.ar>
2022-11-03 09:22:16 -0300

    hola

---
+++ src/components/Posts.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import {auth, db} from '../firebase/config';
+
+class Posts extends Component {
+    constructor(){
+        super();
+        this.state ={
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return(
+            <View>
+                <Text>
+                    Hola
+                </Text>
+            </View>
+        )
+    }
+}
