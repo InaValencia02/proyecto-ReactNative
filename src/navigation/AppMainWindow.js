1dffe1c4193cf9c51306cf478264bf52551b7b3b
carocanorio <carocanorio@gmail.com>
2022-11-07 10:03:14 -0300

    arregle algo del login. Falta camra

--- src/navigation/AppMainWindow.js
+++ src/navigation/AppMainWindow.js
@@ -3,8 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import AddPosts from "../screens/AddPosts";
-import { Ionicons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
+import { Ionicons, Entypo } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -19,9 +18,9 @@ class AppMainWindow extends Component{
     render(){
         return(
             <Tab.Navigator>
-                <Tab.Screen name='Profile' component={Profile} option={{tabBarIcon: () => <Ionicons name="person" size={24} color="black" />}} />
-                <Tab.Screen name='Add new post' component={AddPosts} option={{tabBarIcon: () => <Ionicons name="md-add-circle-outline" size={24} color="black" /> }}/>
-                <Tab.Screen name='Home' component={Home} option={{tabBarIcon: () => <Entypo name="home" size={24} color="black" />}} />
+                <Tab.Screen name='Home' component={Home} options={{tabBarIcon: () => <Entypo name="home" size={24} color="black" />}} />
+                <Tab.Screen name='Add new post' component={AddPosts} options={{tabBarIcon: () => <Ionicons name="md-add-circle-outline" size={24} color="black" /> }}/>
+                <Tab.Screen name='Profile' component={Profile} options={{tabBarIcon: () => <Ionicons name="person" size={24} color="black" />}} />
             </Tab.Navigator>
         )
     }
