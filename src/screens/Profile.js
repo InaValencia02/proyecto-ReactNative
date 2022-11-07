c529b3948017e2221d7ccf1bccdfdcdac85c20ab
quillenbucciero <abuccierorodriguez@udesa.edu.ar>
2022-11-07 10:12:27 -0300

    update profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -8,16 +8,11 @@ class Profile extends Component{
     constructor () {
         super();
         this.state = {
-            userName: '',
-            newPassword: '',
-            bio: ''
+            
         }
     }
 
-    updateProfile() {
-        db.collection('users')
-
-    }
+    
 
     render () {
 
@@ -25,31 +20,8 @@ class Profile extends Component{
 
             <View>
 
-                <Text>Update Profile</Text>
-
-                <View style={styles.cardContainer}>
-                    <TextInput keyboardType='default'
-                        placeholder='New user name'
-                        onChangeText={(text) => { this.setState({ userName: text }) }}
-                        value={this.state.userName}
-                        style={styles.cardText}
-                    />
-                    <TextInput keyboardType='password'
-                        placeholder='New password'
-                        onChangeText={(text) => { this.setState({ newPassword: text }) }}
-                        value={this.state.newPassword}
-                        style={styles.cardText}
-                    />
-                    <TextInput keyboardType='default'
-                        placeholder='My bio'
-                        onChangeText={(text) => { this.setState({ bio: text }) }}
-                        value={this.state.bio}
-                        style={styles.cardText}
-                    />
-                    <TouchableOpacity onPress={() => this.updateProfile()} >
-                        <Text style={styles.like} >Update</Text>
-                    </TouchableOpacity>
-                </View>
+                
+                
 
             </View>
 
