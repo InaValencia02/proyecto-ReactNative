995dd43437f4f66934ed99bf65dbcb9d69433806
quillenbucciero <abuccierorodriguez@udesa.edu.ar>
2022-11-07 10:08:13 -0300

    likes

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,23 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import {auth, db} from '../firebase/config';
+import { auth, db } from '../firebase/config';
 
 class Posts extends Component {
-    constructor(){
-        super();
-        this.state ={
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            liked: this.props.post.data.likes.includes(auth.currentUser.email)
         }
     }
 
     componentDidMount() {
 
     }
+    like() {
+
+        db.collection('posteos')
+            .doc(this.props.post.id)
+            .update({
+                likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+            })
+            .then(() => {
+                this.setState({ liked: true })
+            })
+            .catch(error => console.log(error))
+
+
+    }
+    dislike() {
+        db.collection('posteos')
+            .doc(this.props.post.id)
+            .update({
+                likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+            })
+            .then(() => {
+                this.setState({ liked: false })
+            })
+            .catch(e => console.log(e))
+
+    }
 
 
     render() {
-        return(
+        return (
             <View style={styles.postContainer}>
+
+
+
+                <Text >This many people {this.props.post.data.likes.length} liked this post</Text>
+
+                {this.state.liked ?
+                    <TouchableOpacity onPress={() => this.dislike()}>
+                        <Text style={styles.like}> <AntDesign name="heart" size={24} color="red" /> </Text>
+                    </TouchableOpacity>
+                    :
+                    <TouchableOpacity onPress={() => this.like()}>
+                        <Text style={styles.like}> <AntDesign name="hearto" size={24} color="black" /> </Text>
+                    </TouchableOpacity>
+                }
+                
 
             </View>
         )
