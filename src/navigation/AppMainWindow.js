c0060a005bf32fb40487612ccd73132aab23cb11
carocanorio <carocanorio@gmail.com>
2022-11-07 10:13:42 -0300

    componente search

--- src/navigation/AppMainWindow.js
+++ src/navigation/AppMainWindow.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import AddPosts from "../screens/AddPosts";
+import Search from '../screens/Search';
 import { Ionicons, Entypo } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
@@ -20,6 +21,7 @@ class AppMainWindow extends Component{
             <Tab.Navigator>
                 <Tab.Screen name='Home' component={Home} options={{tabBarIcon: () => <Entypo name="home" size={24} color="black" />}} />
                 <Tab.Screen name='Add new post' component={AddPosts} options={{tabBarIcon: () => <Ionicons name="md-add-circle-outline" size={24} color="black" /> }}/>
+                <Tab.Screen name='Search' component={Search} options={{tabBarIcon: () => <Ionicons name="search-circle" size={24} color="black" />}} />
                 <Tab.Screen name='Profile' component={Profile} options={{tabBarIcon: () => <Ionicons name="person" size={24} color="black" />}} />
             </Tab.Navigator>
         )
