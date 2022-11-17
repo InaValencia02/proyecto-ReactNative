cf7f6fd6513035ba614f188019ba1efb5f77856d
carocanorio <ccanoriohellwing@udesa.edu.ar>
2022-11-17 19:07:15 -0300

    boton de confirmación

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -12,6 +12,8 @@ class Posts extends Component {
             comment: '',
             emptyComment: '',
             posts: [],
+            deleteMessage: '',
+            delete: false,
 
         }
     }
@@ -57,8 +59,16 @@ class Posts extends Component {
         this.props.navigation.navigate("OtherProfile", {user: user})
     }    
 
+    deleteMessage(){
+        this.setState({deleteMessage: 'Are you sure you want to delete this post?', delete: true})
+    }
+
     deletePost(){
         db.collection('posts').doc(this.props.post.id).delete()
+    }
+
+    notDelete(){
+        this.setState({deleteMessage: '', delete: false})
     }
 
     render() {
@@ -150,9 +160,25 @@ class Posts extends Component {
 
                     {
                         this.props.post.data.owner == auth.currentUser.email ?
-                        <TouchableOpacity onPress={() => this.deletePost()}>
-                            <Text style={styles.deletebutton}>Delete post</Text>
-                        </TouchableOpacity>
+                        <>
+                            <TouchableOpacity onPress={() => this.deleteMessage()}>
+                                <Text style={styles.deletebutton}>Delete post</Text>
+                            </TouchableOpacity>
+
+                             <Text style={styles.deleteMessage}>{this.state.deleteMessage}</Text>
+                            {this.state.delete  ?
+                             <View style={styles.deleteContainer}>
+                                <TouchableOpacity onPress={() => this.deletePost()}>
+                                    <Text style={styles.confirmationButton}>Yes</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => this.notDelete()}>
+                                    <Text style={styles.denialButton}>No</Text>
+                                </TouchableOpacity>
+                             </View>
+                            :
+                            <></>
+                             }
+                        </>
                         :
                         <></>
                     }
@@ -248,6 +274,41 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 15,
         color: 'white',
+    },
+    confirmationButton: {
+        marginTop: '2%',
+        margin: '2%',
+        padding: '1%',
+        textAlign: 'center',
+        fontSize: 14,
+        color: 'black',
+        borderColor: 'black',
+        borderRadius: '30px',
+        backgroundColor: '#e6e6e6'
+    },
+    denialButton: {
+        margin: '2%',
+        marginTop: '1%',
+        padding: '1%',
+        textAlign: 'center',
+        fontSize: 14,
+        color: 'black',
+        borderRadius: '30px',
+        backgroundColor: '#e6e6e6'
+    },
+    deleteMessage: {
+        marginTop: '7%',
+        marginBottom: '-5%',
+        fontSize: 15,
+        fontWeight: 'bold',
+    },
+    deleteContainer: {
+        borderRadius: 10,
+        borderWidth: 1,
+        margin: 10,
+        textAlign: 'left',
+        padding: 10,
+        borderColor: 'white',
     }
 });
 
